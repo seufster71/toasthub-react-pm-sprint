359a05f7a1377680c1ad1d7a7b52aa3ac4223fb3
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-06-07 12:47:02 -0400

    pm buildout

--- sprint-container.js
+++ sprint-container.js
@@ -177,7 +177,7 @@ class PMSprintContainer extends Component {
 				id = item.id;
 			}
 			fuLogger.log({level:'TRACE',loc:'SprintContainer::onModify',msg:"test"+id});
-			this.props.actions.modifyItem(id);
+			this.props.actions.modifyItem({id,appPrefs:this.props.appPrefs});
 		};
 	}
 	
