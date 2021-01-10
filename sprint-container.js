d8d03435c5ce6e98f05d4e6c708d9e7f09f65659
Edward Seufert <edwardseufert@Edwards-MBP.home>
2021-01-10 16:59:23 -0500

    cleanup

--- sprint-container.js
+++ sprint-container.js
@@ -22,263 +22,37 @@ import * as sprintActions from './sprint-actions';
 import fuLogger from '../../core/common/fu-logger';
 import SprintView from '../../memberView/pm_sprint/sprint-view';
 import SprintModifyView from '../../memberView/pm_sprint/sprint-modify-view';
-import utils from '../../core/common/utils';
-import moment from 'moment';
+import BaseContainer from '../../core/container/base-container';
 
-
-class PMSprintContainer extends Component {
+class PMSprintContainer extends BaseContainer {
 	constructor(props) {
 		super(props);
-		this.state = {pageName:"PM_SPRINT",isDeleteModalOpen: false, errors:null, warns:null, successes:null};
-		this.onListLimitChange = this.onListLimitChange.bind(this);
-		this.onSearchClick = this.onSearchClick.bind(this);
-		this.onSearchChange = this.onSearchChange.bind(this);
-		this.onPaginationClick = this.onPaginationClick.bind(this);
-		this.onOrderBy = this.onOrderBy.bind(this);
-		this.openDeleteModal = this.openDeleteModal.bind(this);
-		this.closeModal = this.closeModal.bind(this);
-		this.onSave = this.onSave.bind(this);
-		this.onModify = this.onModify.bind(this);
-		this.onDelete = this.onDelete.bind(this);
-		this.onEditRoles = this.onEditRoles.bind(this);
-		this.inputChange = this.inputChange.bind(this);
-		this.onCancel = this.onCancel.bind(this);
-		this.onBlur = this.onBlur.bind(this);
-		this.clearVerifyPassword = this.clearVerifyPassword.bind(this);
 	}
 
 	componentDidMount() {
 		this.props.actions.init();
 	}
-
-	onListLimitChange(fieldName) {
-		return (event) => {
-			let value = 20;
-			if (this.props.codeType === 'NATIVE') {
-				value = event.nativeEvent.text;
-			} else {
-				value = event.target.value;
-			}
-
-			let listLimit = parseInt(value);
-			this.props.actions.listLimit({state:this.props.pmsprint,listLimit});
-		};
-	}
-
-	onPaginationClick(value) {
-		return(event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onPaginationClick',msg:"fieldName "+ value});
-			let listStart = this.props.pmsprint.listStart;
-			let segmentValue = 1;
-			let oldValue = 1;
-			if (this.state["PM_SPRINT_PAGINATION"] != null && this.state["PM_SPRINT_PAGINATION"] != ""){
-				oldValue = this.state["PM_SPRINT_PAGINATION"];
-			}
-			if (value === "prev") {
-				segmentValue = oldValue - 1;
-			} else if (value === "next") {
-				segmentValue = oldValue + 1;
-			} else {
-				segmentValue = value;
-			}
-			listStart = ((segmentValue - 1) * this.props.pmsprint.listLimit);
-			this.setState({"PM_SPRINT_PAGINATION":segmentValue});
-			
-			this.props.actions.list({state:this.props.pmsprint,listStart});
-		};
-	}
-
-	onSearchChange(fieldName) {
-		return (event) => {
-			if (event.type === 'keypress' && event.key === 'Enter') {
-				this.searchClick(fieldName,event);
-			} else {
-				if (this.props.codeType === 'NATIVE') {
-					this.setState({[fieldName]:event.nativeEvent.text});
-				} else {
-					this.setState({[fieldName]:event.target.value});
-				}
-			}
-		};
-	}
-
-	onSearchClick(fieldName) {
-		return (event) => {
-			this.searchClick(fieldName,event);
-		};
+	
+	getState = () => {
+		return this.props.pmsprint;
 	}
 	
-	searchClick(fieldName,event) {
-		let searchCriteria = [];
-		if (fieldName === 'PM_SPRINT-SEARCHBY') {
-			if (event != null) {
-				for (let o = 0; o < event.length; o++) {
-					let option = {};
-					option.searchValue = this.state['PM_SPRINT-SEARCH'];
-					option.searchColumn = event[o].value;
-					searchCriteria.push(option);
-				}
-			}
-		} else {
-			for (let i = 0; i < this.props.pmsprint.searchCriteria.length; i++) {
-				let option = {};
-				option.searchValue = this.state['PM_SPRINT-SEARCH'];
-				option.searchColumn = this.props.pmsprint.searchCriteria[i].searchColumn;
-				searchCriteria.push(option);
+	getForm = () => {
+		return "PM_SPRINT_FORM";
+	}
+	
+	onOption = (code,item) => {
+		fuLogger.log({level:'TRACE',loc:'PMTeamContainer::onOption',msg:" code "+code});
+		if (this.onOptionBase(code,item)) {
+			return;
+		}
+		
+		switch(code) {
+			case 'EDIT_ROLE': {
+				this.onEditRoles(item);
+				break;
 			}
 		}
-
-		this.props.actions.search({state:this.props.pmsprint,searchCriteria});
-	}
-
-	onOrderBy(selectedOption) {
-		return (event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onOrderBy',msg:"id " + selectedOption});
-			let orderCriteria = [];
-			if (event != null) {
-				for (let o = 0; o < event.length; o++) {
-					let option = {};
-					if (event[o].label.includes("ASC")) {
-						option.orderColumn = event[o].value;
-						option.orderDir = "ASC";
-					} else if (event[o].label.includes("DESC")){
-						option.orderColumn = event[o].value;
-						option.orderDir = "DESC";
-					} else {
-						option.orderColumn = event[o].value;
-					}
-					orderCriteria.push(option);
-				}
-			} else {
-				let option = {orderColumn:"PM_SPRINT_TABLE_NAME",orderDir:"ASC"};
-				orderCriteria.push(option);
-			}
-			this.props.actions.orderBy({state:this.props.pmsprint,orderCriteria});
-		};
-	}
-	
-	onSave() {
-		return (event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onSave',msg:"test"});
-			let errors = utils.validateFormFields(this.props.pmsprint.prefForms.PM_SPRINT_FORM,this.props.pmsprint.inputFields);
-			
-			if (errors.isValid){
-				this.props.actions.saveItem({state:this.props.pmsprint});
-			} else {
-				this.setState({errors:errors.errorMap});
-			}
-		};
-	}
-	
-	onModify(item) {
-		return (event) => {
-			let id = null;
-			if (item != null && item.id != null) {
-				id = item.id;
-			}
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onModify',msg:"test"+id});
-			this.props.actions.modifyItem({id,appPrefs:this.props.appPrefs});
-		};
-	}
-	
-	onDelete(item) {
-		return (event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onDelete',msg:"test"});
-			this.setState({isDeleteModalOpen:false});
-			if (item != null && item.id != "") {
-				this.props.actions.deleteItem({state:this.props.pmsprint,id:item.id});
-			}
-		};
-	}
-	
-	openDeleteModal(item) {
-		return (event) => {
-		    this.setState({isDeleteModalOpen:true,selected:item});
-		}
-	}
-	
-	onEditRoles(item) {
-		return (event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onEditRoles',msg:"test"+item.id});
-			this.props.history.push({pathname:'/admin-roles',state:{parent:item}});
-		};
-	}
-	
-	closeModal() {
-		return (event) => {
-			this.setState({isDeleteModalOpen:false,errors:null,warns:null});
-		};
-	}
-	
-	onCancel() {
-		return (event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onCancel',msg:"test"});
-			this.props.actions.list({state:this.props.pmsprint});
-		};
-	}
-	
-	inputChange(fieldName,switchValue) {
-		return (event) => {
-			let value = "";
-			if (switchValue === "DATE") {
-				value = event.toISOString();
-			} else {
-				value = switchValue;
-			}
-			utils.inputChange(this.props,fieldName,value);
-		};
-	}
-	
-	onBlur(field) {
-		return (event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::onBlur',msg:field.name});
-			let fieldName = field.name;
-			// get field and check what to do
-			if (field.optionalParams != ""){
-				let optionalParams = JSON.parse(field.optionalParams);
-				if (optionalParams.onBlur != null) {
-					if (optionalParams.onBlur.validation != null && optionalParams.onBlur.validation == "matchField") {
-						if (field.validation != "") {
-							let validation = JSON.parse(field.validation);
-							if (validation[optionalParams.onBlur.validation] != null && validation[optionalParams.onBlur.validation].id != null){
-								if (this.props.pmsprint.inputFields[validation[optionalParams.onBlur.validation].id] == this.props.pmsprint.inputFields[fieldName]) {
-									if (validation[optionalParams.onBlur.validation].successMsg != null) {
-										let successMap = this.state.successes;
-										if (successMap == null){
-											successMap = {};
-										}
-										successMap[fieldName] = validation[optionalParams.onBlur.validation].successMsg;
-										this.setState({successes:successMap, errors:null});
-									}
-								} else {
-									if (validation[optionalParams.onBlur.validation].failMsg != null) {
-										let errorMap = this.state.errors;
-										if (errorMap == null){
-											errorMap = {};
-										}
-										errorMap[fieldName] = validation[optionalParams.onBlur.validation].failMsg;
-										this.setState({errors:errorMap, successes:null});
-									}
-								}
-							}
-						}
-					} else if (optionalParams.onBlur.func != null) {
-						if (optionalParams.onBlur.func == "clearVerifyPassword"){
-							this.clearVerifyPassword();
-						}
-					}
-				}
-			}
-			
-		};
-	}
-	
-	clearVerifyPassword() {
-	//	return (event) => {
-			fuLogger.log({level:'TRACE',loc:'SprintContainer::clearVerifyPassword',msg:"Hi there"});
-			this.setState({errors:null, successes:null});
-			this.props.actions.clearField('PM_SPRINT_FORM_VERIFY_PASSWORD');
-	//	}
 	}
 
 	render() {
@@ -286,21 +60,16 @@ class PMSprintContainer extends Component {
 		if (this.props.pmsprint.isModifyOpen) {
 			return (
 				<SprintModifyView
-				containerState={this.state}
-				item={this.props.pmsprint.selected}
-				inputFields={this.props.pmsprint.inputFields}
+				itemState={this.props.pmsprint}
 				appPrefs={this.props.appPrefs}
-				itemPrefForms={this.props.pmsprint.prefForms}
 				onSave={this.onSave}
 				onCancel={this.onCancel}
-				onReturn={this.onCancel}
 				inputChange={this.inputChange}
-				onBlur={this.onBlur}/>
+				/>
 			);
 		} else if (this.props.pmsprint.items != null) {
 			return (
 				<SprintView
-				containerState={this.state}
 				itemState={this.props.pmsprint}
 				appPrefs={this.props.appPrefs}
 				onListLimitChange={this.onListLimitChange}
@@ -308,11 +77,8 @@ class PMSprintContainer extends Component {
 				onSearchClick={this.onSearchClick}
 				onPaginationClick={this.onPaginationClick}
 				onOrderBy={this.onOrderBy}
-				openDeleteModal={this.openDeleteModal}
 				closeModal={this.closeModal}
-				onModify={this.onModify}
-				onDelete={this.onDelete}
-				onEditRoles={this.onEditRoles}
+				onOption={this.onOption}
 				inputChange={this.inputChange}
 				session={this.props.session}
 				/>
--- sprint-reducer.js
+++ sprint-reducer.js
@@ -31,8 +31,15 @@ export default function usersReducer(state = {}, action) {
 					listStart: reducerUtils.getListStart(action),
 					orderCriteria: [{'orderColumn':'PM_SPRINT_TABLE_NAME','orderDir':'ASC'}],
     				searchCriteria: [{'searchValue':'','searchColumn':'PM_SPRINT_TABLE_NAME'}],
+    				paginationSegment: 1,
 					selected: null,
-					isModifyOpen: false
+					isModifyOpen: false,
+					pageName:"PMSPRINT",
+					isDeleteModalOpen: false,
+					errors:null, 
+					warns:null, 
+					successes:null,
+					searchValue:""
 				});
 			} else {
 				return state;
@@ -45,8 +52,13 @@ export default function usersReducer(state = {}, action) {
 					items: reducerUtils.getItems(action),
 					listLimit: reducerUtils.getListLimit(action),
 					listStart: reducerUtils.getListStart(action),
+					paginationSegment: action.paginationSegment,
 					selected: null,
-					isModifyOpen: false
+					isModifyOpen: false,
+					isDeleteModalOpen: false,
+					errors:null, 
+					warns:null, 
+					successes:null
 				});
 			} else {
 				return state;
@@ -118,6 +130,22 @@ export default function usersReducer(state = {}, action) {
 		}
 		case 'PM_SPRINT_ORDERBY': { 
 			return reducerUtils.updateOrderBy(state,action);
+		}
+		case 'PM_SPRINT_SET_ERRORS': {
+			return Object.assign({}, state, {
+				errors: action.errors
+			});
+		}
+		case 'PM_SPRINT_CLOSE_DELETE_MODAL': {
+			return Object.assign({}, state, {
+				isDeleteModalOpen: false
+			});
+		}
+		case 'PM_SPRINT_OPEN_DELETE_MODAL': {
+			return Object.assign({}, state, {
+				isDeleteModalOpen: true,
+				selected: action.item
+			});
 		}
 		default:
 			return state;
